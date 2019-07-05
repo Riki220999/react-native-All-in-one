d2173a70c71270ab3646881448c5b36649f3c27c
Riki setiyo Pambudi <setiyoriki589@gmail.com>
2019-07-06 02:42:19 +0700

    app js

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * @author: dwi.setiyadi@gmail.com
+*/
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Bootstrap from './app/bootstrap';
+
+export default class Aplication extends Component {
+  render() {
+    return (
+      <Provider store={Bootstrap.ReduxStore}>
+        <PersistGate
+          loading={null}
+          persistor={Bootstrap.ReduxPersist}
+        >
+          <Bootstrap.Container.View />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
